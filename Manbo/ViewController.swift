8502b37e96ed5ae074f15e6026f8acef23927b0a
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-13 20:43:08 +0900

    ui 생성

--- Manbo/ViewController.swift
+++ Manbo/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    var circularView: CircularProgressView!
+    var duration: TimeInterval!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        circularView.center = view.center
+        containerView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        view.addSubview(circularView)
     }
 
-
+    @objc func handleTap() {
+        duration = 5    //Play with whatever value you want :]
+        circularView.progressAnimation(duration: duration)
+    }
 }
-
